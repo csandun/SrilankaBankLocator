ff815a0a5e70f6adfe92568f16bc61431125e591
chathurangasandun <chathurangasandukumara@gmail.com>
2016-02-01 04:38:32 +0530

    add search input box

--- app/src/main/java/com/mikepenz/materialdrawer/app/BranchFragment.java
+++ app/src/main/java/com/mikepenz/materialdrawer/app/BranchFragment.java
@@ -1,11 +1,13 @@
 package com.mikepenz.materialdrawer.app;
 
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,12 +19,41 @@ public class BranchFragment extends Fragment {
         // Required empty public constructor
     }
 
+    View v;
+    EditText etSearchBranch;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_branck, container, false);
+        v =  inflater.inflate(R.layout.fragment_branck, container, false);
+        etSearchBranch = (EditText) v.findViewById(R.id.etSearchBranch);
+
+
+        etSearchBranch.setOnTouchListener(new View.OnTouchListener() {
+            final int DRAWABLE_LEFT = 0;
+            final int DRAWABLE_TOP = 1;
+            final int DRAWABLE_RIGHT = 2;
+            final int DRAWABLE_BOTTOM = 3;
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    int leftEdgeOfRightDrawable = etSearchBranch.getRight()
+                            - etSearchBranch.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width();
+                    // when EditBox has padding, adjust leftEdge like
+                    // leftEdgeOfRightDrawable -= getResources().getDimension(R.dimen.edittext_padding_left_right);
+                    if (event.getRawX() >= leftEdgeOfRightDrawable) {
+                        // clicked on clear icon
+                        etSearchBranch.setText("");
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+
+
+        return  v;
     }
 
 
